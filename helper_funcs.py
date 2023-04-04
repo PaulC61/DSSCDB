d175d1244406cfa47a3b8edea35c2c36fc0c29ea
PaulC61 <paulclarke61@gmail.com>
2023-04-04 16:41:44 +0300

    dye-device-QSAR

--- helper_funcs.py
+++ helper_funcs.py
@@ -23,26 +23,25 @@ def pca_dashboard(original_data, pca_data, pca, hue):
     ax_3D.set_ylabel("PC2")
     ax_3D.set_zlabel("PC3")
     ax_3D.dist=9
-    # fig.colorbar(points)
 
-    # explained_variance_ratio = pd.DataFrame(data=pca.explained_variance_ratio_, index=["PC1", "PC2", "PC3"])
+    explained_variance_ratio = pd.DataFrame(data=pca.explained_variance_ratio_, index=["PC1", "PC2", "PC3"])
 
-    # ax_evr = fig.add_subplot(2,2,3)
-    # sns.barplot(ax=ax_evr, data=explained_variance_ratio.transpose())
-    # ax_evr.set(
-    #     ylabel="Explained Variance Ratio"
-    # )
+    ax_evr = fig.add_subplot(2,2,3)
+    sns.barplot(ax=ax_evr, data=explained_variance_ratio.transpose())
+    ax_evr.set(
+        ylabel="Explained Variance Ratio"
+    )
 
-    # loading_score_matrix = pca.components_.T * np.sqrt(pca.explained_variance_)
+    loading_score_matrix = pca.components_.T * np.sqrt(pca.explained_variance_)
 
-    # loading_scores = pd.DataFrame(data=loading_score_matrix, columns=["PC1", "PC2", "PC3"], index=original_data.columns)
+    loading_scores = pd.DataFrame(data=loading_score_matrix, columns=["PC1", "PC2", "PC3"], index=original_data.columns)
 
-    # most_influential = pd.concat([loading_scores['PC1'].sort_values(axis='rows').head(5), loading_scores['PC1'].sort_values(axis='rows').tail(5)], axis='index')
+    most_influential = pd.concat([loading_scores['PC1'].sort_values(axis='rows').head(5), loading_scores['PC1'].sort_values(axis='rows').tail(5)], axis='index')
 
-    # ax_ls = fig.add_subplot(2,2,4)
-    # most_influential.plot.barh(ax=ax_ls)
-    # ax_ls.set(
-    #     xlabel="Loading Scores"
-    # )
+    ax_ls = fig.add_subplot(2,2,4)
+    most_influential.plot.barh(ax=ax_ls)
+    ax_ls.set(
+        xlabel="Loading Scores"
+    )
 
     plt.tight_layout()
